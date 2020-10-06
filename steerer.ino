bffb877dc5f27331d015a296f2293d7102d43d44
matandoocorpo <72224012+matandoocorpo@users.noreply.github.com>
2020-10-06 10:32:51 +0200

    Update steerer.ino
    Updated header

--- steerer.ino
+++ steerer.ino
@@ -1,3 +1,22 @@
+/*
+    Zwift Steering Simulator
+    Takes ADC reading from pin 32 and maps it to an angle from -40 to 40
+    Then transmits it to Zwift via BLE
+    
+    Inspired in samples by Kolban (ESP32 and BLE Arduino) & Peter Everett
+    Thanks to Keith Wakeham's for his protocol explanation https://www.youtube.com/watch?v=BPVFjz5zD4g
+    Thanks to fiveohhh for the demo code https://github.com/fiveohhh/zwift-steerer/
+    
+    Written on Arduino IDE 1.8.13
+    
+    Tested using Zwift on iPad, iPhone, MacBook and iMac, should work on other platforms
+    
+    v0.1 Oct 2020 matandoocorpo / EA1NK
+    
+    Licensed under GNU GPL-3
+
+*/
+
 //Libraries
 
 #include <Arduino.h>
